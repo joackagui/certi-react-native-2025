09811ad41732da71f846e3c7a9f3ff2170024980
Paul Landaeta <paul.landaeta@coxautoinc.com>
2025-10-21 11:32:40 -0400

    feat: firebase store tr

--- sessions/08-feria/app/(tabs)/stores.tsx
+++ sessions/08-feria/app/(tabs)/stores.tsx
@@ -1,16 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Vendors } from '../../src/components/Vendors';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { fetchVendors } from '../../src/services/vendorService';
+import { fetchVendors, subscribeVendors } from '../../src/services/vendorService';
 
 const StoreScreen = () => {
+    const [vendorsTR, setVendorsTR] = useState([]);
     const fetchValues = async () =>{
         const vendors = await fetchVendors();
         console.log(JSON.stringify(vendors));
     }
     useEffect(()=>{
       fetchValues();
+      const unSub = subscribeVendors( rows => {
+        setVendorsTR(rows as any)
+      });
+     
+      return () => unSub();
     }, []) 
+    console.log('timereal', JSON.stringify(vendorsTR));
     return (
         <SafeAreaView>
             <Vendors />
